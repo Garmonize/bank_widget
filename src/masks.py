c4c0ffea0dc6828b6a7b2234131d44b3e0bb7328
archie <mak@MacBook-Pro-mak.local>
2025-01-13 21:03:40 +0300

    adding new files

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску.
+    Номер карты замаскирован и отображается в формате XXXX XX** **** XXXX, где X — это цифра номера
+    """
+    formatted_card_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+    return formatted_card_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция get_mask_account принимает на вход номер счета и возвращает его маску.
+    Номер счета замаскирован и отображается в формате **XXXX, где X — это цифра номера.
+    """
+    formatted_account_number = f"**{account_number[-4:]}"
+    return formatted_account_number
+
+
+print(get_mask_card_number(input()))
+print(get_mask_account(input()))
