4632563e683f9231f56eb1e2a70db39a1f7c4ce3
Archie <garmonize@gmail.com>
2025-01-19 02:04:49 +0300

    minor fixes

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.fixture
+def input_card_number_fix():
+    return '5674746564736453'
+
+
+@pytest.fixture
+def result_card_number_fix():
+    return '5674 74** **** 6453'
+
