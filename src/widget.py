d9f3a595d77f1dcebdced8845ea0d53ebc20effb
Archie <garmonize@gmail.com>
2025-01-19 02:24:06 +0300

    linters deployed

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from masks import get_mask_account, get_mask_card_number
+from masks import get_mask_account_number, get_mask_card_number
 
 
 def mask_account_card(card_info: str) -> str:
@@ -8,7 +8,7 @@ def mask_account_card(card_info: str) -> str:
     в формате Visa Platinum 7000 79** **** 6361 или Счет **4305"""
     formatted = card_info.split(" ")  # создается список из строки, разделенный пробелами
     if "Счет" in formatted:
-        masked_account_number = get_mask_account(formatted[1])
+        masked_account_number = get_mask_account_number(formatted[1])
         return f"{formatted[0]} {masked_account_number}"
     elif len(formatted) > 2:
         masked_card_number = get_mask_card_number(formatted[2])
@@ -20,5 +20,5 @@ def mask_account_card(card_info: str) -> str:
 
 def get_date(raw_date: str) -> str:
     """Функция принимает строку с необработанной датой, очищает ее и возвращает строку в формате ДД.ММ.ГГГГ"""
-    date_obj = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%S.%f')
-    return date_obj.strftime('%d.%m.%Y')
+    date_obj = datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,59 +1,61 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account_number
+from src.masks import get_mask_account_number, get_mask_card_number
 
 
-def test_get_mask_card_number_with_fixtures(input_card_number_fix, result_card_number_fix):
+def test_get_mask_card_number_with_fixtures(input_card_number_fix: str, result_card_number_fix: str) -> None:
     assert get_mask_card_number(input_card_number_fix) == result_card_number_fix
 
 
 @pytest.mark.parametrize(
-    'input_card_number, expected_card_number',
+    "input_card_number, expected_card_number",
     [
-        ('5674746564736453', '5674 74** **** 6453'),
-        ('1234123412341234', '1234 12** **** 1234'),
+        ("5674746564736453", "5674 74** **** 6453"),
+        ("1234123412341234", "1234 12** **** 1234"),
     ],
 )
-def test_get_mask_card_number(input_card_number, expected_card_number):
+def test_get_mask_card_number(input_card_number: str, expected_card_number: str) -> None:
     assert get_mask_card_number(input_card_number) == expected_card_number
 
 
 @pytest.mark.parametrize(
-    'wrong_card_number',
+    "wrong_card_number",
     [
         None,
-        '728473287587540238475093827540875',
-        '78364',
-        'ajhsvbf897198709871234',
-    ]
+        "728473287587540238475093827540875",
+        "78364",
+        "ajhsvbf897198709871234",
+    ],
 )
-def test_get_mask_card_number_error(wrong_card_number):
+def test_get_mask_card_number_error(wrong_card_number: str) -> None:
     with pytest.raises(TypeError) as exc_card_info:
         get_mask_card_number(wrong_card_number)
 
-    assert str(exc_card_info.value) == 'Введите корректный номер карты. Номер должен состоять из 16 цифр, без пробелов'
+    assert str(exc_card_info.value) == "Введите корректный номер карты. Номер должен состоять из 16 цифр, без пробелов"
+
 
 @pytest.mark.parametrize(
-    'input_account_number, expected_account_number',
-    [
-        ('73654108430135874305', '**4305'),
-        ('76172634987162398476', '**8476')
-    ]
+    "input_account_number, expected_account_number",
+    [("73654108430135874305", "**4305"), ("76172634987162398476", "**8476")],
 )
-def test_get_mask_account_number(input_account_number, expected_account_number):
+def test_get_mask_account_number(input_account_number: str, expected_account_number: str) -> None:
     assert get_mask_account_number(input_account_number) == expected_account_number
 
+
 @pytest.mark.parametrize(
-    'wrong_account_number',
+    "wrong_account_number",
     [
         None,
-        '87687697869889718972458787147',
-        '981724',
-        'huijkb1249809843',
-    ]
+        "87687697869889718972458787147",
+        "981724",
+        "huijkb1249809843",
+    ],
 )
-def test_get_mask_account_number_error(wrong_account_number):
+def test_get_mask_account_number_error(wrong_account_number: str) -> None:
     with pytest.raises(TypeError) as exc_account_info:
         get_mask_account_number(wrong_account_number)
 
-    assert str(exc_account_info.value) == 'Введите корректный номер счета. Номер должен быть состоять из 20 цифр, без пробелов'
+    assert (
+        str(exc_account_info.value)
+        == "Введите корректный номер счета. Номер должен быть состоять из 20 цифр, без пробелов"
+    )
