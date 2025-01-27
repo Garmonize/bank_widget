6df16c9a90545740408545d4e38970d839356e64
Archie <garmonize@gmail.com>
2025-01-27 13:50:56 +0300

    tests for generators module

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,14 +1,9 @@
 import pytest
 from typing import Any
 
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
-# написать тесты если:
-# если все ровно,
-# совсем нет данных,
-# запрашиваемая валюта не найдена,
-# не соблюден регистр
 @pytest.mark.parametrize(
     "transactions, expected",
     [
@@ -71,10 +66,39 @@ def test_wrong_type_input(wrong_input: Any) -> None:
 
     assert str(exp_info.value) == "Данные переданы неверно"
 
+@pytest.mark.parametrize(
+    "normal_input, expect_result",
+    [
+        (
+            [
+                {
+            "id": 555555555,
+            "state": "EXECUTED",
+            "date": "2023-03-22T22:22:22.222222",
+            "operationAmount": {"amount": "12345.67", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Пополнение счета",
+            "from": "Счет 99887766554433221100",
+            "to": "Счет 11223344556677889900",
+                }
+            ],
+            [
+                "Пополнение счета"
+            ],
+        )
+    ]
+)
+def test_transaction_descriptions(normal_input, expect_result) -> None:
+    """функция проверяет правильность вывода значений при обычной проверке"""
+    assert list(transaction_descriptions(normal_input)) == expect_result
 
-def test_no_currensy_found():
-    pass
 
+@pytest.mark.parametrize(
+    "begin, end, result",
+    [
+        (1, 2, ["0000 0000 0000 0001", "0000 0000 0000 0002"])
+    ]
+)
+def test_card_number_generator(begin, end, result) -> None:
+    """функция проверяет генерацию номера карты при заданных значениях диапазона"""
+    assert list(card_number_generator(begin, end)) == result
 
-def test_bad_register_entered():
-    pass
